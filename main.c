06d925732ac02e60d89b0deef55e8268e7b8bebc
Paula Biazotto <pds_paula@hotmail.com>
2023-03-29 12:26:47 +0100

    initial main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+// not needed when using generate_test_runner.rb
+int main(void) {
+    UNITY_BEGIN();
+
+    return UNITY_END();
+}
+
