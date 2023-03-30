d531a62752231937e845eba1f71278755fe9f4e4
Paula Biazotto <pds_paula@hotmail.com>
2023-03-30 11:15:45 +0100

    tests 01 ft_printf with stdio.h

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 NAME = libftprintf.a
 
-SRCS =  
+SRCS =  ft_printf.c
 
 OBJS = $(SRCS:.c=.o)
 
--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,49 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 12:27:43 by paula             #+#    #+#             */
-/*   Updated: 2023/03/29 13:19:44 by paula            ###   ########.fr       */
+/*   Updated: 2023/03/30 11:07:44 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
+
+void replace(char st, va_list ap)
+{
+    if (st == '%')
+    {
+        putchar('%');
+    }
+    if (st == 's')
+    {
+        puts(va_arg(ap, char*));
+    }
+}
 
 int ft_printf(const char *st, ...)
 {
+    int     ret;
+    int     i;
+    va_list ap;
 
-    return(0);
+    ret = 0;
+    i = 0;
+    va_start(ap, st);
+    while (st[i] != 0)
+    {
+        if (st[i] == '%')
+        {
+            replace(st[++i], ap);
+        }
+        else
+            write(1, &st[i], 1);
+        i++;
+    }
+    va_end(ap);
+    return(ret);
+}
+
+int main(void)
+{
+    ft_printf("ola %s %s \n", "mundo", "bonito");
 }
